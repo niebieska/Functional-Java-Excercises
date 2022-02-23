009bef19ea909ca27d23d9277a247951ab2fe3e2
Paulina Wątroba <paulina@watroba.com.pl>
2022-02-23 12:05:23 +0100

    Functional Interfaces resolvings

--- src/exercises/FunctionalInterfaces.java
+++ src/exercises/FunctionalInterfaces.java
@@ -3,6 +3,7 @@ package exercises;
 import common.DataFactory;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.*;
@@ -13,46 +14,90 @@ public class FunctionalInterfaces {
 
     public static void main(String[] args) {
 
-        // UnaryOperator
+        // unaryoperator
+
+        UnaryOperator<Integer> unaryOutput = a -> a * 5;
+        Integer a = 5;
+
+        System.out.println(unaryOutput.apply(a));
+
         /**
-         * Please create a UnaryOperator<Integer> instance that returns its parameter multiplied by 5.
-         * After that please use it and return the result.
+         * please create a unaryoperator<integer> instance that returns its parameter multiplied by 5.
+         * after that please use it and return the result.
          */
         // code
 
-        // BinaryOperator
+        // binaryoperator
         /**
-         * Please create a BinaryOperator<BigInteger> instance that returns the sum of its parameters.
-         * After that please use it and return the result.
+         * please create a binaryoperator<biginteger> instance that returns the sum of its parameters.
+         * after that please use it and return the result.
          */
         // code
+        BinaryOperator<BigInteger> binaryOperator = BigInteger::add;
+        System.out.println(binaryOperator.apply(BigInteger.ONE, BigInteger.TWO));
 
-        // Predicate
+        // predicate
         /**
-         * Please create a Predicate<String> instance that determines whether passed parameter is empty
-         * After that please use it and return the result.
+         * please create a predicate<string> instance that determines whether passed parameter is empty
+         * after that please use it and return the result.
          */
         // code
+        Predicate<String> stringPredicate = String::isEmpty;
 
-        // Function
+        System.out.println(stringPredicate.test(""));
+        // function
         /**
-         * Please create a Function<List<String>,<String>> instance that will take a list of strings,
+         * please create a function<list<string>,<string>> instance that will take a list of strings,
          * convert it to array and then convert it to a string.
-         * After that please use it and return the result.
+         * after that please use it and return the result.
+         */
+        // code
+        Function<List<String>,String> stringToArray = list -> {
+            System.out.println(list);
+            return Arrays.toString(list.toArray());
+        };
+        String convertedList = stringToArray.apply(Arrays.asList("Ala", "ma", "kota"));
+
+        System.out.println(convertedList);
+        System.out.printf("here is string: %s", convertedList);
+
+        System.out.println(genericStringToArraySecond(Arrays.asList("Sierotka", "ma", "rysia")) );
+        System.out.println(genericStringToArraySecond(Arrays.asList(1,2,3)) );
+
+        // supplier
+        /**
+         * please create a supplier<string> that would call on getsuppliedtext() method.
+         * after that please use it and return the result.
+         */
+
+        Supplier<String> supplier = DataFactory::getSuppliedText;
+        String suppliedText = supplier.get();
+        // code
+
+        // consumer
+        /**
+         * please create a consumer<string> that would display the parameter on a console
          */
         // code
 
-        // Supplier
-        /**
-         * Please create a Supplier<String> that would call on getSuppliedText() method.
-         * After that please use it and return the result.
-         */
-        // code
-
-        // Consumer
-        /**
-         * Please create a Consumer<String> that would display the parameter on a console
-         */
-        // code
+        Consumer<String> consumer = s -> System.out.println("A parameter on console: \t" + s);
+        consumer.accept("it's works !");
     }
+
+    public static <T> Function<List<T>, String> genericStringToArray(List<T> list) {
+
+        Function<List<T>,String> stringToArray = list1 -> {
+            System.out.println(list1);
+            return Arrays.toString(list1.toArray()); };
+
+
+        return  stringToArray;
+
+    }
+
+    public  static <T> String genericStringToArraySecond(List<T> list) {
+
+        return   Arrays.toString(list.toArray()); }
 }
+
+
