1f95255792ab3253d0c331f0e29d30eaad5b6943
Paulina Wątroba <paulina@watroba.com.pl>
2022-02-23 12:04:11 +0100

    Streams resolvings

--- src/exercises/Streams.java
+++ src/exercises/Streams.java
@@ -7,6 +7,7 @@ import common.model.MovieSeries;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static common.DataFactory.*;
@@ -15,13 +16,12 @@ public class Streams {
 
     /**
      * Please complete all of below exercises using Stream API and data from @DataFactory class
-     *
+     * <p>
      * For data sources please use following methods:
-     *
+     * <p>
      * book list - getBooks() method
      * movie series list - getMovieSeries() method
      * random words generator - getSomeRandomWords() method
-     *
      */
     public static void main(String[] args) {
 
@@ -29,50 +29,61 @@ public class Streams {
         /**
          * Please return a list of book titles from provided book list
          */
-        List<String> bookNames = null;
+        List<String> bookNames = getBooks().stream().map(Book::getTitle).collect(Collectors.toList());
+        //System.out.println(bookNames);
 
+        for (String s : bookNames
+        ) {
+            System.out.println(s);
+
+        }
         // flatmap
         /**
          * Please return a list of all available movies from provided movie series list
          */
-        List<Movie> availableMovies = null;
+        List<Movie> availableMovies = getMovieSeries().stream().flatMap(Collection::stream).collect(Collectors.toList());
+        System.out.println("\n Flatmap ");
+        for (Movie movie : availableMovies
+        ) {
+            System.out.println(movie.getTitle());
 
+        }
         // filter
         /**
          * Please return a list of all books written by J_K_ROWLING from provided book list
-         */
-        List<Book> booksByJKRowling = null;
+         */ Predicate <Book> bookPredicate = book -> book.getAuthor() == Author.J_K_ROWLING;
+        List<Book> booksByJKRowling = getBooks().stream().filter(bookPredicate).collect(Collectors.toList());
+        //List<Book> booksByJKRowling = getBooks().stream().filter(book -> book.getAuthor() == Author.J_K_ROWLING).collect(Collectors.toList());
 
-        // count
+        // countList<Book> booksByJKRowling = getBooks().stream().filter(book -> book.getAuthor() == Author.J_K_ROWLING).collect(Collectors.toList());
         /**
          * Please return the amount of books written by T_PRATCHETT from provided book list
          */
-        long booksByTerryPratchettCount = 0;
+        long booksByTerryPratchettCount = getBooks().stream().filter(book -> book.getAuthor() == Author.J_K_ROWLING).count();
 
         // findFirst - nicer effect with parallel stream
         /**
          * Please find first available STAR_WARS movie from provided movie series list
          */
-        Optional<Movie> firstStarWarsMovieOnTheShelf = null;
+        Optional<Movie> firstStarWarsMovieOnTheShelf = getMovieSeries().stream().flatMap(Collection::stream).filter(movie -> movie.getSeries() == MovieSeries.STAR_WARS).findFirst();
 
         // findAny - nicer effect with parallel stream
         /**
          * Please find any book written by A_SAPKOWSKI from provided book list
          */
-        Optional<Book> anyBookByASapkowski = null;
+        Optional<Book> anyBookByASapkowski = getBooks().parallelStream().filter(book -> book.getAuthor() == Author.A_SAPKOWSKI).findAny();
 
 
         // toList
         /**
          * Please create a list of all movie title from provided movie list
          */
-        List<String> movieTitlesList = null;
-
+        List<String> movieTitlesList = getMovieSeries().stream().flatMap(Collection::stream).map(Movie::getTitle).collect(Collectors.toList());
         // toSet
         /**
          * Please create a set of all authors from provided book list
          */
-        Set<Author> bookAuthors = null;
+        Set<Author> bookAuthors = getBooks().stream().map(Book::getAuthor).collect(Collectors.toSet());
 
         // toMap
         /**
@@ -81,24 +92,25 @@ public class Streams {
          * !only one book per author is required
          * !if new book is found for the same author it should replace the previous one
          */
-        Map<Author, Book> bookOfAnAuthorMap = null;
+        Map<Author, Book> bookOfAnAuthorMap = getBooks().stream().collect(Collectors.toMap(Book::getAuthor, Function.identity(), (Book existing, Book replacement) -> existing));
 
         // groupingBy
         /**
          * Please create a map of books with author as a key from provided book list
          */
-        Map<Author, Set<Book>> booksGroupedByAuthor = null;
+        Map<Author, Set<Book>> booksGroupedByAuthor = getBooks().stream().collect(Collectors.groupingBy(Book::getAuthor,Collectors.toSet()));
 
         // joining
         /**
          * Please create a string from provided random words generator, using " " (space) as a delimiter
          */
-        String totallyRandomSentence = null;
+        String totallyRandomSentence = getSomeRandomWords().stream().map(String::toUpperCase).collect(Collectors.joining(""));
 
         // forEach - only to display results!
         /**
          * Please display any of the results on the console using forEach() method and a Consumer<T> functional interface
          */
+        availableMovies.forEach(movie -> System.out.println(movie.getTitle()));
         // code
     }
 }
